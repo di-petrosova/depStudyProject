df4e912d2344fa1fb251a1df115bee1b784fdfd5
Diana <petrosova.di@gmail.com>
2020-11-02 13:43:36 +0200

    slf4j logger was added

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -1,10 +1,15 @@
 package com.application;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main
 {
+    private static Logger LOGGER = LoggerFactory.getLogger("Main.class");
 
     public static void main(String[] args)
     {
+        LOGGER.error("Error message");
         System.out.println("Hello, world!");
     }
 
