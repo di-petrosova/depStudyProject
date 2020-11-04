28fec6ad297af8580192f30110379c61d430edc2
Diana <petrosova.di@gmail.com>
2020-11-04 14:24:04 +0200

    Creating service for getting properties from project.properties

--- src/main/java/com/application/Main.java
+++ src/main/java/com/application/Main.java
@@ -10,7 +10,6 @@ public class Main
     public static void main(String[] args)
     {
         LOGGER.error("Error message");
-        System.out.println("Hello, world!");
         Hello hello = new Hello();
     }
 
